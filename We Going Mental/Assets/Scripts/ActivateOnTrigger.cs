421a9e7f41d490bc88f2fd8c5b9b4f440d696dad
Brandon Howe <cyberchroma@hotmail.com>
2020-03-08 10:45:28 -0400

    Chapters Set Up

---
+++ We Going Mental/Assets/Scripts/ActivateOnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateOnTrigger : MonoBehaviour
+{
+    public GameObject objectToActivate;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        objectToActivate.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
